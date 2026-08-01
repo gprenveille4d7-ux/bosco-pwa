bf4f6a4bfe7d5d3db4b01abd0f850cb46cbfb395
gprenveille4d7-ux <g.prenveille4d7@gmail.com>
2026-08-02 00:22:59 +0200

    Point Bosco v35 startup to the published app bundle

--- assets/standalone-v27.js
+++ assets/standalone-v27.js
@@ -3,7 +3,7 @@ const mount = document.getElementById("bosco-root");
 try {
   const framework = await import("./framework-CXnKph_e.js");
   const runtime = await import("./rolldown-runtime-S-ySWqyJ.js");
-  const page = await import("./page-iOpbtbK4.js");
+  const page = await import("./page-DDq_2qKE.js");
   const React = runtime.r(framework.i(), 1);
   const ReactDOMClient = framework.t();
   mount.replaceChildren();
