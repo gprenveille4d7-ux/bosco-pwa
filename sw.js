9fff006447f606d1b9d6127a8edc465e20e9ea7c
gprenveille4d7-ux <g.prenveille4d7@gmail.com>
2026-08-09 19:36:31 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "bosco-pwa-v28-gemini-3.5";
+const CACHE_NAME = "bosco-pwa-v28-gemini-3.6";
 const CORE_ASSETS = [
   "/bosco-pwa/",
   "/bosco-pwa/manifest.webmanifest",
   "/bosco-pwa/assets/index-CCACixLy.css",
   "/bosco-pwa/assets/page-B7dK8B51.css",
-  "/bosco-pwa/assets/ui-navigation-v28-13.css",
+  "/bosco-pwa/assets/ui-navigation-v28-14.css",
   "/bosco-pwa/assets/standalone-v27.js",
   "/bosco-pwa/assets/standalone-v28.js",
   "/bosco-pwa/assets/standalone-v28-8.js",
