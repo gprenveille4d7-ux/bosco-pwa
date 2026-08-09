84c4d40d902375e0d5b7c141454f199233d94d1d
gprenveille4d7-ux <g.prenveille4d7@gmail.com>
2026-08-09 22:43:30 +0200

    Refactor service worker for caching and fetch handling

--- sw.js
+++ sw.js
@@ -42,7 +42,7 @@ const CORE_ASSETS = [
   "/bosco-pwa/data/carnet-emile/barfleur.js",
   "/bosco-pwa/data/carnet-emile/saint-vaast.js",
   "/bosco-pwa/data/carnet-emile/quineville.js",
-
+  "/bosco-pwa/data/carnet-emile/utah-beach.js",
 
 
 
